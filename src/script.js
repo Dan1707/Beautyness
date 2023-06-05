67c60071ac76c4c9c1b425e7117f2f86d0e8fa6c
Dan1707 <danuhae@gmail.com>
2023-06-05 18:14:40 +0300

    first commit

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
 
-    var panel = this.nextElementSibling;
+    let panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
     } else {
